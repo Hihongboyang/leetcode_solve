f4559020002957cfa4c14c9298f45a022112a5d3
hongbo <245035255@qq.com>
2021-06-08 11:06:59 +0800

    2021-06-08 面试金典3.4 普通解法

--- 面试金典3.4.c
+++ 面试金典3.4.c
@@ -1,169 +1,144 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-typedef struct Element_
+typedef struct
 {
-    int point;  // 存储当栈中数据的位置
-    int order;  // 标记当前为第几个栈
-    int *value; // 使用数组存储数据
-    struct Element_ *next;
-    struct Element_ *pre;
-} Stack_Element;
+    int size;
+    int push_point;
+    int pop_point;
 
-typedef struct StackOfPlates_
+    int *push_value;
+    int *pop_value;
+} MyQueue;
+
+/** Initialize your data structure here. */
+
+MyQueue *myQueueCreate()
 {
-    int size; // 保存每个栈的大小
-    Stack_Element *head;
-    Stack_Element *tail;
+    MyQueue *queue = (MyQueue *)malloc(sizeof(MyQueue));
+    int *push = (int *)malloc(sizeof(int) * 100);
+    int *pop = (int *)malloc(sizeof(int) * 100);
 
-} StackOfPlates;
+    queue->pop_point = 0;
+    queue->push_point = 0;
+    queue->pop_value = pop;
+    queue->push_value = push;
+    queue->size = 100;
 
-StackOfPlates *stackOfPlatesCreate(int cap)
-{
-    StackOfPlates *stack = (StackOfPlates *)malloc(sizeof(StackOfPlates));
-    Stack_Element *element = (Stack_Element *)malloc(sizeof(Stack_Element));
-    int *list = cap > 0 ? (int *)malloc(sizeof(int) * cap) : NULL;
-    element->value = list;
-    element->point = 0;
-    element->next = NULL;
-    element->pre = NULL;
-    element->order = 0;
-
-    stack->size = cap;
-    stack->head = element;
-    stack->tail = element;
-    return stack;
+    return queue;
 }
 
-void stackOfPlatesPush(StackOfPlates *obj, int val)
+/** Push element x to the back of queue. */
+void myQueuePush(MyQueue *obj, int x)
 {
-    if (obj->size < 1)
+    if (obj == NULL)
     {
         return;
     }
-    if (obj->tail == NULL || obj->tail->point >= obj->size)
+    if (obj->push_point == obj->size)
     {
-        Stack_Element *element = (Stack_Element *)malloc(sizeof(Stack_Element));
-        int *list = obj->size > 0 ? (int *)malloc(sizeof(int) * obj->size) : NULL;
-        list[0] = val;
-        element->value = list;
-        element->point = 1;
-        element->next = NULL;
-        if (obj->tail == NULL)
-        {
-            element->order = 0;
-        }
-        else
-        {
-            element->order = obj->tail->order + 1;
-        }
-
-        element->pre = obj->tail;
-        if (obj->tail != NULL)
-            obj->tail->next = element;
-        obj->tail = element;
-        if (obj->head == NULL)
-        {
-            obj->head = element;
-        }
+        return;
     }
-    else
-    {
-        obj->tail->value[obj->tail->point] = val;
-        obj->tail->point++;
-    }
+    obj->push_value[obj->push_point] = x;
+    obj->push_point++;
 }
 
-int stackOfPlatesPop(StackOfPlates *obj)
+/** Removes the element from in front of queue and returns that element. */
+int myQueuePop(MyQueue *obj)
 {
-    int ret_val;
-    if (obj->tail == NULL || obj->size < 1)
+    if (obj == NULL)
     {
         return -1;
     }
-
-    if (obj->tail->point >= 1)
+    int ret_val;
+    if (obj->pop_point >= 1)
     {
-        ret_val = obj->tail->value[obj->tail->point - 1];
-        obj->tail->point--;
-        if (obj->tail->point == 0 && obj->tail->order != 0) // 检查是不是第一个栈，保证总有一个栈存在。
+        ret_val = obj->pop_value[obj->pop_point - 1];
+        obj->pop_point--;
+    }
+
+    else if (obj->pop_point == 0 && obj->push_point >= 1)
+    {
+        while (obj->push_point >= 1)
         {
-            obj->tail = obj->tail->pre;
-            obj->tail->next = NULL;
+            obj->pop_value[obj->pop_point] = obj->push_value[obj->push_point - 1];
+            obj->pop_point++;
+            obj->push_point--;
         }
+        ret_val = obj->pop_value[obj->pop_point - 1];
+        obj->pop_point--;
     }
     else
     {
-        ret_val = -1;
+        return;
     }
     return ret_val;
 }
 
-int stackOfPlatesPopAt(StackOfPlates *obj, int index)
+/** Get the front element. */
+int myQueuePeek(MyQueue *obj)
 {
-    int ret_val;
-    Stack_Element *p = obj->head;
-    
-
-    while (p != NULL && p->order != index)
+    if (obj == NULL)
     {
-        // 寻找index的位置
-        p = p->next;
-    }
-    if (p == NULL || obj->size < 1)
-    {
-        // index超出了当前栈数量
         return -1;
     }
+    int ret_val;
 
-    // 找到对应的栈了，
-    if (p->point >= 1)
+    if (obj->pop_point < 1)
     {
-        ret_val = p->value[p->point - 1];
-        p->point--;
-        if (p->point == 0 && p->order != 0) // 检查是不是第一个栈，保证总有一个栈存在。
+        if (obj->push_point < 1)
         {
-            p->pre->next = p->next;
-            if (p->next != NULL)
-            {
-                p->next->order--;
-                p->next->pre = p->pre;
-            }
-            else
-            {
-                obj->tail = p->pre;
-            }
+            return -1;
         }
+        ret_val = obj->push_value[0];
     }
     else
     {
-        ret_val = stackOfPlatesPop(obj);
+        ret_val = obj->pop_value[obj->pop_point - 1];
     }
 
     return ret_val;
 }
 
-void stackOfPlatesFree(StackOfPlates *obj)
+/** Returns whether the queue is empty. */
+bool myQueueEmpty(MyQueue *obj)
 {
+    return (obj->pop_point < 1) && (obj->push_point < 1);
+}
+
+void myQueueFree(MyQueue *obj)
+{
+    free(obj->pop_value);
+    free(obj->push_value);
     free(obj);
 }
 
 int main(int argc, char const *argv[])
 {
-    // 内存消耗较少，但是耗时较长。使用，数组的化耗时应该会下降。
-    StackOfPlates *stack;
+    // 使用两个列表完成一个队列
+    MyQueue *queue;
     int val;
-    stack = stackOfPlatesCreate(2);
+    queue = myQueueCreate();
 
-    stackOfPlatesPush(stack, -2);
-    stackOfPlatesPush(stack, 0);
-    stackOfPlatesPush(stack, -1);
+    myQueuePush(queue, -2);
+    myQueuePush(queue, 0);
+    myQueuePush(queue, -1);
 
-    val = stackOfPlatesPop(stack);
-    printf("%d ", val);
-    val = stackOfPlatesPopAt(stack, 0);
-    printf("%d ", val);
-    val = stackOfPlatesPop(stack);
-    printf("%d ", val);
+    printf("%d ", myQueuePeek(queue));
+    printf("%d ", myQueuePop(queue));
+    printf("%d ", myQueuePeek(queue));
+
+    myQueuePush(queue, 4);
+    myQueuePush(queue, 5);
+    myQueuePush(queue, 66);
+    printf("%d ", myQueuePop(queue));
+    printf("%d ", myQueuePop(queue));
+    printf("%d ", myQueuePop(queue));
+    printf("%d ", myQueueEmpty(queue));
+    printf("%d ", myQueuePop(queue));
+    printf("%d ", myQueuePop(queue));
+    printf("%d ", myQueueEmpty(queue));
+
     return 0;
 }
