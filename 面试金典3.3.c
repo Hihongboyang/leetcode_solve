752395d246139e28ee070a5f258be13e16a541a3
hongbo <245035255@qq.com>
2021-06-08 11:06:27 +0800

    2021-06-08 修改 之前的错误

--- 面试金典3.3.c
+++ 面试金典3.3.c
@@ -1,121 +1,169 @@
 #include <stdlib.h>
-#include <limits.h>
+#include <stdio.h>
 
-// 使用两个栈实现 获取栈中的最小值。
-typedef struct element_
+typedef struct Element_
 {
-    int val;
-    struct element_ *next;
-}Element;
+    int point;  // 存储当栈中数据的位置
+    int order;  // 标记当前为第几个栈
+    int *value; // 使用数组存储数据
+    struct Element_ *next;
+    struct Element_ *pre;
+} Stack_Element;
 
-typedef struct
+typedef struct StackOfPlates_
 {
-    int size;
-    Element *head;
-    Element *tail;
-    Element *head_min;
-    Element *tail_min;
+    int size; // 保存每个栈的大小
+    Stack_Element *head;
+    Stack_Element *tail;
 
-} MinStack;
+} StackOfPlates;
 
-#define stack_size(stack) ((stack)->size)
-
-/** initialize your data structure here. */
-
-MinStack *minStackCreate()
+StackOfPlates *stackOfPlatesCreate(int cap)
 {
-    MinStack *stack = (MinStack *)malloc(sizeof(MinStack));
-    Element *new_element = (Element *)malloc(sizeof(Element));
-    Element *new_element_min = (Element *)malloc(sizeof(Element));
-    new_element->val = -1;
-    new_element->next = NULL;
+    StackOfPlates *stack = (StackOfPlates *)malloc(sizeof(StackOfPlates));
+    Stack_Element *element = (Stack_Element *)malloc(sizeof(Stack_Element));
+    int *list = cap > 0 ? (int *)malloc(sizeof(int) * cap) : NULL;
+    element->value = list;
+    element->point = 0;
+    element->next = NULL;
+    element->pre = NULL;
+    element->order = 0;
 
-    new_element_min->val = INT_MAX;
-    new_element_min->next = NULL;
-
-    stack->size = 0;
-    stack->head = new_element;
-    stack->head_min = new_element_min;
-    stack->tail = new_element;
-    stack->tail_min = new_element_min;
+    stack->size = cap;
+    stack->head = element;
+    stack->tail = element;
     return stack;
 }
 
-void minStackPush(MinStack *obj, int x)
+void stackOfPlatesPush(StackOfPlates *obj, int val)
 {
-    Element *new_element = (Element *)malloc(sizeof(Element));
-    Element *new_element_min = (Element *)malloc(sizeof(Element));
-
-    // 头插法
-    new_element->val = x;
-    new_element->next = obj->head;
-    obj->head = new_element;
-    obj->size++;
-
-    new_element_min->val = (x < obj->head_min->val) ? x: obj->head_min->val;
-    new_element_min->next = obj->head_min;
-    obj->head_min = new_element_min;
-}
-
-void minStackPop(MinStack *obj)
-{
-    if (stack_size(obj) == 0)
+    if (obj->size < 1)
     {
         return;
     }
+    if (obj->tail == NULL || obj->tail->point >= obj->size)
+    {
+        Stack_Element *element = (Stack_Element *)malloc(sizeof(Stack_Element));
+        int *list = obj->size > 0 ? (int *)malloc(sizeof(int) * obj->size) : NULL;
+        list[0] = val;
+        element->value = list;
+        element->point = 1;
+        element->next = NULL;
+        if (obj->tail == NULL)
+        {
+            element->order = 0;
+        }
+        else
+        {
+            element->order = obj->tail->order + 1;
+        }
 
-    Element *temp;
-    temp = obj->head;
-    obj->head = obj->head->next;
-    free(temp);
-
-    temp = obj->head_min;
-    obj->head_min = obj->head_min->next;
-    free(temp);
-    obj->size--;
+        element->pre = obj->tail;
+        if (obj->tail != NULL)
+            obj->tail->next = element;
+        obj->tail = element;
+        if (obj->head == NULL)
+        {
+            obj->head = element;
+        }
+    }
+    else
+    {
+        obj->tail->value[obj->tail->point] = val;
+        obj->tail->point++;
+    }
 }
 
-int minStackTop(MinStack *obj)
+int stackOfPlatesPop(StackOfPlates *obj)
 {
-    if (stack_size(obj) == 0)
+    int ret_val;
+    if (obj->tail == NULL || obj->size < 1)
     {
         return -1;
     }
-    return obj->head->val;
+
+    if (obj->tail->point >= 1)
+    {
+        ret_val = obj->tail->value[obj->tail->point - 1];
+        obj->tail->point--;
+        if (obj->tail->point == 0 && obj->tail->order != 0) // 检查是不是第一个栈，保证总有一个栈存在。
+        {
+            obj->tail = obj->tail->pre;
+            obj->tail->next = NULL;
+        }
+    }
+    else
+    {
+        ret_val = -1;
+    }
+    return ret_val;
 }
 
-int minStackGetMin(MinStack *obj)
+int stackOfPlatesPopAt(StackOfPlates *obj, int index)
 {
-    if (stack_size(obj) == 0)
+    int ret_val;
+    Stack_Element *p = obj->head;
+    
+
+    while (p != NULL && p->order != index)
     {
+        // 寻找index的位置
+        p = p->next;
+    }
+    if (p == NULL || obj->size < 1)
+    {
+        // index超出了当前栈数量
         return -1;
     }
 
-    return obj->head_min->val;
+    // 找到对应的栈了，
+    if (p->point >= 1)
+    {
+        ret_val = p->value[p->point - 1];
+        p->point--;
+        if (p->point == 0 && p->order != 0) // 检查是不是第一个栈，保证总有一个栈存在。
+        {
+            p->pre->next = p->next;
+            if (p->next != NULL)
+            {
+                p->next->order--;
+                p->next->pre = p->pre;
+            }
+            else
+            {
+                obj->tail = p->pre;
+            }
+        }
+    }
+    else
+    {
+        ret_val = stackOfPlatesPop(obj);
+    }
+
+    return ret_val;
 }
 
-void minStackFree(MinStack *obj)
+void stackOfPlatesFree(StackOfPlates *obj)
 {
     free(obj);
 }
 
-
 int main(int argc, char const *argv[])
 {
-
-    MinStack *stack;
+    // 内存消耗较少，但是耗时较长。使用，数组的化耗时应该会下降。
+    StackOfPlates *stack;
     int val;
-    stack = minStackCreate();
+    stack = stackOfPlatesCreate(2);
 
-    minStackPush(stack, -2);
-    minStackPush(stack, 0);
-    minStackPush(stack, -1);
+    stackOfPlatesPush(stack, -2);
+    stackOfPlatesPush(stack, 0);
+    stackOfPlatesPush(stack, -1);
 
-    val = minStackGetMin(stack);
+    val = stackOfPlatesPop(stack);
     printf("%d ", val);
-    minStackTop(stack);
-    minStackTop(stack);
-    val = minStackGetMin(stack);
+    val = stackOfPlatesPopAt(stack, 0);
+    printf("%d ", val);
+    val = stackOfPlatesPop(stack);
     printf("%d ", val);
     return 0;
 }
