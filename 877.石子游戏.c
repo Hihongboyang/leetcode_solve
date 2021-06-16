48e5f1dc6e51943056207c297b1a945289d1fef8
hongbo <245035255@qq.com>
2021-06-16 21:36:26 +0800

    leetcode 打卡修改

--- 877.石子游戏.c
+++ 877.石子游戏.c
@@ -12,38 +12,14 @@
 // 按照题目的意思是只有 亚历克斯无论如何都应赢不了的时候才返回false
 // 那么亚历克斯 每次都取最大的数， 而李每次都取最小的数，如果这样亚历克斯都
 // 赢不了，那么就返回false。但是感觉这样有问题。
+
+// 看到题解，将数列以位置的奇偶区分，分为第一组和第二组。由于题目说，石子的总数是奇数 没有平局。
+// 所以第一组和第二组中，总有一组的和 会比另一组大。而亚历克斯作为第一个选择的人，总是可以选择
+// 和大的那一组。
+// 5，3，4，5
+// 偶 奇 偶 奇
 bool stoneGame(int* piles, int pilesSize){
-    int left = 0, right = pilesSize - 1, ya = 0, li = 0;
-    for (int i = 0; i<pilesSize; i++)
-    {
-        if(i % 2 == 0)
-        {
-            if (piles[left] > piles[right])
-            {
-                ya += piles[left];
-                left++;
-            }
-            else
-            {
-                ya += piles[right];
-                right--;
-            }
-        }
-        else
-        {
-            if (piles[left] < piles[right])
-            {
-                li += piles[left];
-                left++;
-            }
-            else
-            {
-                li += piles[right];
-                right--;
-            }
-        }
-    }
-    return ya > li;
+    return true;
 }
 // @lc code=end
 
